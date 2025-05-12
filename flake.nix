5069a787fc968eef429932f1c44a62e3f8b2cf66
randomdude16671 <randomdude16671@proton.me>
2025-05-12 13:09:23 +0530

    Added some things for development

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Dev shell with clang, raylib, clang-tools, and bear";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }: 
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.go
+            pkgs.golangci-lint
+            pkgs.go-tools 
+          ];
+
+          shellHook = ''
+            if [ -z "$IN_ZSH" ]; then
+              export IN_ZSH=1
+              exec zsh -i
+            fi
+          '';
+        };
+      });
+}
+
