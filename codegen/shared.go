ab7dd3f0d0a7f886bbb67f96350dce64fc60b251
randomdude16671 <randomdude16671@proton.me>
2025-06-10 22:40:31 +0530

    Update shared.go, started making a small semantic analyzer

--- codegen/shared.go
+++ codegen/shared.go
@@ -1,9 +1,35 @@
 package codegen
 
+import l "github.com/randomdude16671/mini-build/language"
+
+// the target currently being compiled to.
 type CompilerTarget int
 
+// Append to over time
 const (
 	_          CompilerTarget = iota
 	TargetELF                 // Compiles to flat assembly (FASM)
 	TargetBEAM                // Compiles to erlang  (Not implemented.)
 )
+
+// Enum to hold types of symbols
+type SymbolType int
+
+const (
+	_ SymbolType = iota
+	// TODO: Finish this list
+	TaskSymbol
+	VariableSymbol
+)
+
+// Analyzer to put through before codegen
+type SemanticAnalyzer struct {
+	input         *string
+	currentSymbol Symbol    // current symbol under analysis
+	program       l.Program // program node to analyze
+}
+
+type Symbol interface {
+	Type() SymbolType
+	Finished() chan<- bool
+}
