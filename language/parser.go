799d65bb5405d98058598f7f9477e4bef07297e9
randomdude16671 <randomdude16671@proton.me>
2025-07-11 21:00:48 +0530

    Testing phase of the parser begins

--- language/parser.go
+++ language/parser.go
@@ -58,7 +58,6 @@ func NewParser(l *Lexer) *Parser {
 		infixParseFns:  make(map[TokenType]infixParseFn),
 	}
 
-	// Register prefix parse functions
 	p.registerPrefix(IDENT, p.parseIdentifier)
 	p.registerPrefix(STRING, p.parseStringLiteral)
 	p.registerPrefix(NUMBER, p.parseNumberLiteral)
@@ -67,7 +66,6 @@ func NewParser(l *Lexer) *Parser {
 	p.registerPrefix(SHELL, p.parseShellExpression)
 	p.registerPrefix(LPAREN, p.parseGroupedExpression)
 
-	// Register infix parse functions
 	p.registerInfix(PLUS, p.parseInfixExpression)
 	p.registerInfix(MINUS, p.parseInfixExpression)
 	p.registerInfix(SLASH, p.parseInfixExpression)
@@ -83,33 +81,45 @@ func NewParser(l *Lexer) *Parser {
 	p.registerInfix(OR, p.parseInfixExpression)
 	p.registerInfix(CONCAT, p.parseConcatExpression)
 
-	// Initialize peek and current token
 	p.nextToken()
 	p.nextToken()
 
 	return p
 }
 
-func (p *Parser) nextToken() {
-	p.currentToken = p.peekToken
-	p.peekToken = p.l.NextToken()
-
-	// Skip newlines and comments
-	for p.peekToken.Type == NEWLINE || p.peekToken.Type == COMMENT {
-		p.peekToken = p.l.NextToken()
-	}
+// -- Registration --
+func (p *Parser) registerPrefix(tokenType TokenType, fn prefixParseFn) {
+	p.prefixParseFns[tokenType] = fn
 }
 
 func (p *Parser) registerInfix(tokenType TokenType, fn infixParseFn) {
 	p.infixParseFns[tokenType] = fn
 }
 
-func (p *Parser) registerPrefix(tokenType TokenType, fn prefixParseFn) {
-	p.prefixParseFns[tokenType] = fn
+// -- Utility --
+func (p *Parser) nextToken() {
+	p.currentToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+	for p.peekToken.Type == NEWLINE || p.peekToken.Type == COMMENT {
+		p.peekToken = p.l.NextToken()
+	}
 }
 
-func (p *Parser) Errors() []string {
-	return p.errors // strings holding human readable errors (With a lot of newlines yk)
+func (p *Parser) peekTokenIs(token TokenType) bool {
+	return p.peekToken.Type == token
+}
+
+func (p *Parser) currentTokenIs(t TokenType) bool {
+	return p.currentToken.Type == t
+}
+
+func (p *Parser) expectPeek(t TokenType) bool {
+	if p.peekTokenIs(t) {
+		p.nextToken()
+		return true
+	}
+	p.peekError(t)
+	return false
 }
 
 func (p *Parser) peekError(t TokenType) {
@@ -117,102 +127,13 @@ func (p *Parser) peekError(t TokenType) {
 	p.errors = append(p.errors, msg)
 }
 
-func (p *Parser) parseIdentifier() Node {
-	return &Identifier{Value: p.currentToken.Literal}
-}
-
-func (p *Parser) parseStringLiteral() Node {
-	return &StringLiteral{Value: p.currentToken.Literal}
-}
-
-func (p *Parser) parseExecStatement() Node {
-	exec := &ExecStatement{}
-
-	return exec
-}
-
-func (p *Parser) parseStatement() Node {
-	switch p.currentToken.Type {
-	case TASK:
-		return p.parseTaskDefinition()
-	case RUN:
-		return p.parseExecStatement()
-	case IF:
-		return p.parseIfStatement()
-	case FOREACH:
-		return p.parseForEachStatement()
-	case WHILE:
-		return p.parseWhileStatement()
-	case COMPILE:
-		return p.parseCompileStatement()
-	case IDENT:
-		// Check if this is an assignment
-		if p.peekTokenIs(ASSIGN) {
-			return p.parseAssignmentStatement()
-		}
-		// Otherwise it's an expression statement
-		return p.parseExpressionStatement()
-	default:
-		return p.parseExpressionStatement()
-	}
-}
-
-func (p *Parser) parseNumberLiteral() Node {
-	lit := &NumberLiteral{}
-	val, err := strconv.ParseFloat(p.currentToken.Literal, 64)
-	if err != nil {
-		msg := fmt.Sprintf("couldn't parser %q as float.", p.currentToken.Literal)
-		p.errors = append(p.errors, msg)
-		return nil
-	}
-	lit.Value = val
-
-	return lit
-}
-
-func (p *Parser) parseInfixExpression(left Node) Node {
-	expression := &BinaryOperation{
-		Left:     left,
-		Operator: p.currentToken.Literal,
-	}
-	precedence := p.currentPrecedence()
-	p.nextToken()
-	expression.Right = p.parseExpression(precedence)
-	return expression
-}
-
-func (p *Parser) parseExpression(precedence int) Node {
-	prefix := p.prefixParseFns[p.currentToken.Type]
-	if prefix == nil {
-		p.noPrefixParseFnError(p.currentToken.Type)
-		return nil
-	}
-
-	leftExp := prefix()
-
-	for !p.peekTokenIs(SEMICOLON) && !p.peekTokenIs(EOF) && precedence < p.peekPrecedence() {
-		infix := p.infixParseFns[p.peekToken.Type]
-		if infix == nil {
-			return leftExp
-		}
-
-		p.nextToken()
-		leftExp = infix(leftExp)
-	}
-
-	return leftExp
+func (p *Parser) Errors() []string {
+	return p.errors
 }
 
 func (p *Parser) noPrefixParseFnError(tokenType TokenType) {
 	msg := fmt.Sprintf("no prefix parse for %s found", tokenType)
 	p.errors = append(p.errors, msg)
-}
-
-func (p *Parser) peekTokenIs(token TokenType) bool {
-	if p.peekToken.Type == token {
-		return true
-	}
-	return false
 }
 
 func (p *Parser) peekPrecedence() int {
@@ -229,57 +150,220 @@ func (p *Parser) currentPrecedence() int {
 	return LOWEST
 }
 
-func (p *Parser) parseTaskDefinition() *TaskDef {
-	task := &TaskDef{}
+// -- Expressions --
+func (p *Parser) parseExpression(precedence int) Node {
+	prefix := p.prefixParseFns[p.currentToken.Type]
+	if prefix == nil {
+		p.noPrefixParseFnError(p.currentToken.Type)
+		return nil
+	}
 
+	leftExp := prefix()
+
+	for !p.peekTokenIs(SEMICOLON) && !p.peekTokenIs(EOF) && precedence < p.peekPrecedence() {
+		infix := p.infixParseFns[p.peekToken.Type]
+		if infix == nil {
+			return leftExp
+		}
+		p.nextToken()
+		leftExp = infix(leftExp)
+	}
+
+	return leftExp
+}
+
+func (p *Parser) parsePrefixExpression() Node {
+	expression := &UnaryOperation{Operator: p.currentToken.Literal}
+	p.nextToken()
+	expression.Operand = p.parseExpression(LOWEST)
+	return expression
+}
+
+func (p *Parser) parseInfixExpression(left Node) Node {
+	expression := &BinaryOperation{Left: left, Operator: p.currentToken.Literal}
+	precedence := p.currentPrecedence()
+	p.nextToken()
+	expression.Right = p.parseExpression(precedence)
+	return expression
+}
+
+func (p *Parser) parseGroupedExpression() Node {
+	p.nextToken()
+	expr := p.parseExpression(LOWEST)
+	if !p.expectPeek(RPAREN) {
+		return nil
+	}
+	return expr
+}
+
+func (p *Parser) parseConcatExpression(left Node) Node {
+	expr := &ConcatOperation{Left: left}
+	p.nextToken()
+	expr.Right = p.parseExpression(CONCATENTATION)
+	return expr
+}
+
+func (p *Parser) parseIdentifier() Node {
+	return &Identifier{Value: p.currentToken.Literal}
+}
+
+func (p *Parser) parseNumberLiteral() Node {
+	lit := &NumberLiteral{}
+	val, err := strconv.ParseFloat(p.currentToken.Literal, 64)
+	if err != nil {
+		msg := fmt.Sprintf("couldn't parser %q as float.", p.currentToken.Literal)
+		p.errors = append(p.errors, msg)
+		return nil
+	}
+	lit.Value = val
+	return lit
+}
+
+func (p *Parser) parseStringLiteral() Node {
+	return &StringLiteral{Value: p.currentToken.Literal}
+}
+
+func (p *Parser) parseShellExpression() Node {
 	if !p.expectPeek(IDENT) {
 		return nil
 	}
+	return &ShellExpr{Name: p.currentToken.Literal}
+}
 
-	task.Name = p.currentToken.Literal
-
-	// Check for dependencies
-	if p.peekTokenIs(DEPENDENCY) {
-		p.nextToken() // consume DEPENDENCY
-		task.Dependencies = p.parseDependencies()
+// -- Statements --
+func (p *Parser) parseStatement() Node {
+	switch p.currentToken.Type {
+	case TASK:
+		return p.parseTaskDefinition()
+	case RUN:
+		return p.parseExecStatement()
+	case IF:
+		return p.parseIfStatement()
+	case FOREACH:
+		return p.parseForEachStatement()
+	case WHILE:
+		return p.parseWhileStatement()
+	case COMPILE:
+		return p.parseCompileStatement()
+	case IDENT:
+		if p.peekTokenIs(ASSIGN) {
+			return p.parseAssignmentStatement()
+		}
+		return p.parseExpressionStatement()
+	default:
+		return p.parseExpressionStatement()
 	}
+}
 
+func (p *Parser) parseExpressionStatement() Node {
+	return p.parseExpression(LOWEST)
+}
+
+func (p *Parser) parseAssignmentStatement() *AssignmentStatement {
+	assign := &AssignmentStatement{Name: p.currentToken.Literal}
+	if !p.expectPeek(ASSIGN) {
+		return nil
+	}
+	p.nextToken()
+	assign.Value = p.parseExpression(LOWEST)
+	return assign
+}
+
+func (p *Parser) parseExecStatement() *ExecStatement {
+	exec := &ExecStatement{}
+	if !p.expectPeek(IDENT) {
+		return nil
+	}
+	exec.TaskName = p.currentToken.Literal
+	return exec
+}
+
+func (p *Parser) parseIfStatement() *IfStatement {
+	ifStmt := &IfStatement{}
+	p.nextToken()
+	ifStmt.Condition = p.parseExpression(LOWEST)
+
+	if p.peekTokenIs(ELSE) {
+		p.nextToken()
+		if !p.expectPeek(LBRACE) {
+			return nil
+		}
+		ifStmt.ElseBlock = p.parseBlockStatement()
+	}
+	return ifStmt
+}
+
+func (p *Parser) parseForEachStatement() *ForEachStatement {
+	forEach := &ForEachStatement{}
+	if !p.expectPeek(IDENT) {
+		return nil
+	}
+	forEach.VarName = p.currentToken.Literal
+	if !p.expectPeek(STRING) && !p.expectPeek(IDENT) {
+		return nil
+	}
+	forEach.Pattern = p.currentToken.Literal
 	if !p.expectPeek(LBRACE) {
 		return nil
 	}
+	forEach.Body = p.parseBlockStatement()
+	return forEach
+}
 
+func (p *Parser) parseWhileStatement() Node {
+	return &Identifier{Value: "PLACEHOLDER... IN IMPL"}
+}
+
+func (p *Parser) parseCompileStatement() *CompileStatement {
+	cmpStmt := &CompileStatement{}
+	p.nextToken()
+	cmpStmt.File = p.parseExpression(LOWEST)
+	p.nextToken()
+	cmpStmt.Command = p.parseExpression(LOWEST)
+	return cmpStmt
+}
+
+// -- Task Definition --
+func (p *Parser) parseTaskDefinition() *TaskDef {
+	task := &TaskDef{}
+	if !p.expectPeek(IDENT) {
+		return nil
+	}
+	task.Name = p.currentToken.Literal
+	if p.peekTokenIs(DEPENDENCY) {
+		p.nextToken()
+		p.parseDependencies(task)
+	}
+	if !p.expectPeek(LBRACE) {
+		return nil
+	}
 	task.Body = p.parseBlockStatement()
-
 	return task
 }
 
+func (p *Parser) parseDependencies(t *TaskDef) {
+	if !p.expectPeek(IDENT) {
+		return
+	}
+	t.Dependencies = append(t.Dependencies, p.currentToken.Literal)
+	for p.peekTokenIs(COMMA) {
+		p.nextToken()
+		if !p.expectPeek(COMMA) {
+			break
+		}
+		t.Dependencies = append(t.Dependencies, p.currentToken.Literal)
+	}
+}
+
 func (p *Parser) parseBlockStatement() *BlockStatement {
-	block := &BlockStatement{}
-	block.Statements = []Node{}
-
+	block := &BlockStatement{Statements: []Node{}}
 	p.nextToken()
-
-	for !p.currentTokenIs(RBRACE) && p.currentTokenIs(EOF) {
+	for !p.currentTokenIs(RBRACE) && !p.currentTokenIs(EOF) {
 		stmt := p.parseStatement()
 		if stmt != nil {
 			block.Statements = append(block.Statements, stmt)
 		}
 		p.nextToken()
 	}
-
 	return block
-}
-
-func (p *Parser) currentTokenIs(t TokenType) bool {
-	return p.currentToken.Type == t
-}
-
-func (p *Parser) expectPeek(t TokenType) bool {
-	if p.peekTokenIs(t) {
-		p.nextToken()
-		return true
-	} else {
-		p.peekError(t)
-		return false
-	}
 }
